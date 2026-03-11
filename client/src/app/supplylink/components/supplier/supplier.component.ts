2c45236e54e3b9742c25c11ef7bd5d4c91e92b48
ReddyJaiSaiPreetham <dummy@gmail.com>
2026-03-11 18:25:07 +0000

    day19

--- client/src/app/supplylink/components/supplier/supplier.component.ts
+++ client/src/app/supplylink/components/supplier/supplier.component.ts
@@ -1,5 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class SupplierComponent  {
+@Component({
+  selector: 'app-supplier',
+  templateUrl: './supplier.component.html',
+  styleUrls: ['./supplier.component.scss']
+})
+export class SupplierComponent implements OnInit {
 
-  
+  supplierForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.supplierForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.supplierForm.valid) {
+      console.log("Form submitted:", this.supplierForm.value);
+    }
+  }
 }
--- client/src/app/supplylink/services/supplylink.service.ts
+++ client/src/app/supplylink/services/supplylink.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
+// import { environment } from "src/environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Supplier } from "../types/Supplier";
@@ -10,7 +10,7 @@ import { Product } from "../types/Product";
   providedIn: "root",
 })
 export class SupplyLinkService {
-  private baseUrl = `${environment.apiUrl}`;
+  // private baseUrl = `${environment.apiUrl}`;
 
   constructor(private http: HttpClient) { }
 
